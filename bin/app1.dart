1bc0400e7c64d98893f7718821ba116968e323ba
ztzrk <ralif877@gmail.com>
2024-02-23 14:18:13 +0700

    feat/programmer2: add new code on app1

--- bin/app1.dart
+++ bin/app1.dart
@@ -1,3 +1,10 @@
 void main(List<String> arguments) {
   print('Hello world: ini programmer 1!');
+
+  bool featureFlagProgrammer2 = false;
+  if (featureFlagProgrammer2) {
+    //kode yang sudah terupdate
+  } else {
+    //kode yang dari awal sudah ada
+  }
 }
