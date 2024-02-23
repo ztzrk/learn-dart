acef614cadf32facff68950d103dd6531be4a130
ztzrk <ralif877@gmail.com>
2024-02-23 09:19:04 +0700

    first commit

---
+++ bin/app1.dart
@@ -0,0 +1,5 @@
+import 'package:app1/app1.dart' as app1;
+
+void main(List<String> arguments) {
+  print('Hello world: ${app1.calculate()}!');
+}
---
+++ test/app1_test.dart
@@ -0,0 +1,8 @@
+import 'package:app1/app1.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
